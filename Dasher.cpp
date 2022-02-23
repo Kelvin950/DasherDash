17c9f2fee052afb3fc3ec6e16ca6d410c9c46290
Kelvin Baiden <denlinato@gmail.com>
2022-02-23 22:53:54 +0000

    setting up the environment

---
+++ Dasher.cpp
@@ -0,0 +1,17 @@
+#include "raylib.h"
+
+int main(){
+
+
+    InitWindow(400 ,200 , "Dapper Dash" );
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        EndDrawing();
+        /* code */
+    }
+    
+}
